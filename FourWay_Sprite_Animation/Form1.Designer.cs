87820258e13e843af3be71934a5e032eda4e2461
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-05-10 13:12:13 -0400

    Se agregaron recursos más los métodos de la interfaz.

--- FourWay_Sprite_Animation/Form1.Designer.cs
+++ FourWay_Sprite_Animation/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            cantidadMovimientos = new Label();
+            temporizador = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // cantidadMovimientos
+            // 
+            cantidadMovimientos.AutoSize = true;
+            cantidadMovimientos.BackColor = Color.Transparent;
+            cantidadMovimientos.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            cantidadMovimientos.ForeColor = Color.Red;
+            cantidadMovimientos.Location = new Point(10, 495);
+            cantidadMovimientos.Name = "cantidadMovimientos";
+            cantidadMovimientos.Size = new Size(238, 38);
+            cantidadMovimientos.TabIndex = 0;
+            cantidadMovimientos.Text = "Movimientos: 00";
+            // 
+            // temporizador
+            // 
+            temporizador.Enabled = true;
+            temporizador.Interval = 20;
+            temporizador.Tick += TiempoEjecucion;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 544);
+            Controls.Add(cantidadMovimientos);
+            Name = "Form1";
+            Text = "Animaciones en 4 lados";
+            Paint += EdicionFormulario;
+            KeyDown += PresionarCualquierTecla;
+            KeyUp += SoltarCualquierTecla;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label cantidadMovimientos;
+        private System.Windows.Forms.Timer temporizador;
     }
 }
--- FourWay_Sprite_Animation/Form1.cs
+++ FourWay_Sprite_Animation/Form1.cs
@@ -6,5 +6,51 @@ namespace FourWay_Sprite_Animation
         {
             InitializeComponent();
         }
+
+        private void PresionarCualquierTecla(object sender, KeyEventArgs e)
+        {
+            // MÉTODO CUANDO EL JUGADOR PRESIONA UNA TECLA...
+
+            // EN INSTANTES...
+        }
+
+        private void SoltarCualquierTecla(object sender, KeyEventArgs e)
+        {
+            // MÉTODO CUANDO EL JUGADOR SUELTA UNA TECLA...
+
+            // EN INSTANTES...
+        }
+
+        private void EdicionFormulario(object sender, PaintEventArgs e)
+        {
+            // MÉTODO PARA EDITAR EL FORMULARIO MEDIANTE FUNCIONES GRÁFICAS...
+
+            // EN INSTANTES...
+        }
+
+        private void TiempoEjecucion(object sender, EventArgs e)
+        {
+            // MÉTODO PARA INICIALIZAR LA EJECUCIÓN DEL PROYECTO EN GENERAL...
+
+            // EN INSTANTES...
+        }
+
+        // HASTA AHORA DINÁMICAMENTE SE AGREGARON ESTOS EVENTOS...
+
+        // AHORA VAMOS A ESTABLECER AQUÍ LOS SIGUIENTES MÉTODOS DE MANERA ESTÁTICA...
+
+        private void Configuracion()
+        {
+            // MÉTODO SECUENCIAL PARA CONFIGURACIONES POSTERIORES...
+
+            // EN INSTANTES...
+        }
+
+        private void AnimarJugador(int inicial, int final)
+        {
+            // MÉTODO PARA ANIMAR AL JUGADOR SEGÚN LOS TIEMPOS DE EJECUCIÓN DEL PROGRAMA...
+
+            // EN INSTANTES...
+        }
     }
 }
