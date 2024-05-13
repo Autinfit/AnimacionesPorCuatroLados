dea6cc876fbff589fac48cae9d37e6a1c41c13e9
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-05-13 10:33:25 -0400

    Variables creadas en el juego.

--- FourWay_Sprite_Animation/Form1.cs
+++ FourWay_Sprite_Animation/Form1.cs
@@ -2,9 +2,25 @@ namespace FourWay_Sprite_Animation
 {
     public partial class Form1 : Form
     {
+        // ESTE ES UNA CLASE DEL FORMULARIO POR DEFECTO...
+
+        // CREAREMOS ALGUNAS DE SUS VARIABLES PARA ESTE PROYECTO...
+
+        Image jugador; // PARA EL JUGADOR.
+        List<string> movimientosPersonaje = new List<string>(); // MOVIMIENTOS DEL PERSONAJE MEDIANTE UNA LISTA.
+        int pasos = 0; // CANTIDAD DE PASOS QUE MUEVE AQUELLO PERSONAJE.
+        int reducirFrecuenciaPatrones = 0; // REDUCCIÓN DE FRECUENCIA DE PATRONES DEL PROYECTO.
+        bool izquierda, derecha, arriba, abajo; // DECISIÓN DEL USUARIO HACIA DONDE MUEVE EL PERSONAJE.
+        int jugadorEnX; // POSICIÓN DEL JUGADOR EN X DE MANERA HORIZONTAL.
+        int jugadorY; // POSICIÓN DEL JUGADOR EN Y DE MANERA VERTICAL.
+        int alturaJugador = 100; // ALTURA DEL JUGADOR EN EL JUEGO DE MANERA DINÁMICA AQUÍ.
+        int anchoJugador = 100; // ANCHO DEL JUGADOR EN EL JUEGO DE MANERA DINÁMICA AQUÍ.
+        int velocidadJugador = 8; // LA VELOCIDAD DEL JUGADOR SERÁ DE 8 KM.
+
         public Form1()
         {
             InitializeComponent();
+            Configuracion(); // NO OLVIDAR EL LLAMADO DE ESTE MÉTODO AQUÍ.
         }
 
         private void PresionarCualquierTecla(object sender, KeyEventArgs e)
